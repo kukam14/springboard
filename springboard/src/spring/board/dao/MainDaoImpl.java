085ca229e12090751d65a8dbb16f9f4689572019
kukam14 <dorabn@gmail.com>
2014-12-02 23:07:00 +0900

    spring 보드 최초 생성
    최소 생성

---
+++ springboard/src/spring/board/dao/MainDaoImpl.java
@@ -0,0 +1,33 @@
+package spring.board.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.mybatis.spring.support.SqlSessionDaoSupport;
+
+import spring.board.vo.Board;
+
+public class MainDaoImpl extends SqlSessionDaoSupport implements MainDao {
+	
+	public String getToday() {
+		String today =  (String)getSqlSession().selectOne("main.getToday");
+		return today;
+	}
+
+	public int writeProc(Map<String, Object> paramMap) {
+		return getSqlSession().insert("main.writeProc", paramMap);
+	}
+	
+	public int writeProc2(Board board){
+		return getSqlSession().insert("main.writeProc2", board) ;
+	}
+
+	public List getList(Map<String, Object> paramMap) {
+		return getSqlSession().selectList("main.getList");
+	}
+
+	public String getUserPwd(String userid) {
+		return (String)getSqlSession().selectOne("main.getUserPwd", userid);
+	}
+	
+}
